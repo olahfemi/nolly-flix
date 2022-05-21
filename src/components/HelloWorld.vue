b3c813c3ad20c3612e637808e69002c81c5616f9
Femi Olah <olahfemi@gmail.com>
2022-05-21 20:16:55 +0100

    Sort movies by likes in decscending order

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,44 +2,45 @@
   <div class="hello">
     <h1>Welcome to Nolly-Flix</h1>
 
-    <h3>Most Popular Movie</h3>
+
 
     <h3>All Movies</h3>
     <ul>
-      <li v-for="movie in movies" v-bind:key="movie.title">
+      <li v-for="movie in sortedMovies" v-bind:key="movie.title">
         {{movie.title}}
 
         <button @click="movie.likes++">Like ({{movie.likes}})</button>
       </li>
 
     </ul>
-    <h3>Essential Links</h3>
 
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'MovieList',
 
   data(){
     return {
       movies:[
         {title: "The Redemption", likes:0},
-        {title: "Africa’s Tech Roots", likes:0},
-        {title: "The Invisible Man", likes:0},
-        {title: "The Complicated Project", likes:0},
-        {title: "Simply Javascript", likes:0},
+        {title: "Africa’s Tech Roots", likes:4},
+        {title: "The Invisible Man", likes:2},
+        {title: "The Complicated Project", likes:3},
+        {title: "Simply Javascript", likes:6},
         {title: "The New Web", likes:0},
       ]
     }
   },
 
-  props: {
-    msg: String,
-  },
 
-  methods:{
+
+  computed:{
+    sortedMovies(){
+      let moviesCopy = [...this.movies]
+      return moviesCopy.sort((n1,n2) => n2.likes - n1.likes)
+    }
   }
 
 }
